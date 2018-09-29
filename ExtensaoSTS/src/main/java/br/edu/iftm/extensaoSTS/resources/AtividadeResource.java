63b3cb3d6e5b842959b7dab02c10352330ab7733
Arthur201 <arthurlaureanosilva2001@gmail.com>
2018-09-29 11:48:23 -0300

    comitado

--- ExtensaoSTS/src/main/java/br/edu/iftm/extensaoSTS/resources/AtividadeResource.java
+++ ExtensaoSTS/src/main/java/br/edu/iftm/extensaoSTS/resources/AtividadeResource.java
@@ -1,27 +1,25 @@
 package br.edu.iftm.extensaoSTS.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.iftm.extensaoSTS.domain.Atividade;
-import br.edu.iftm.extensaoSTS.repositories.IAtividadeRepository;
+import br.edu.iftm.extensaoSTS.service.AtividadeService;
 
 @RestController
 @RequestMapping(value = "/atividades")
 public class AtividadeResource {
 
 	@Autowired
-	private IAtividadeRepository atividadeRepositorio;
+	private AtividadeService service;
 
-	@GetMapping
-	public List<Atividade> listar() {
-		// Atividade a1 = new Atividade(1, "visita tecnica gdg");
-		// Atividade a2 = new Atividade(2, "simpós");
-		// return Arrays.asList(a1,a2);
-		return atividadeRepositorio.findAll();
+	@RequestMapping(value="{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id){
+		Atividade atividade = service.buscar(id);
+		return ResponseEntity.ok().body(atividade);
 	}
 }
