413c71fe643f6dc8a02e2358ae52d18634cac6cd
Arthur201 <arthurlaureanosilva2001@gmail.com>
2018-09-29 11:29:08 -0300

    comitado

--- ExtensaoSTS/src/main/java/br/edu/iftm/extensaoSTS/domain/Atividade.java
+++ ExtensaoSTS/src/main/java/br/edu/iftm/extensaoSTS/domain/Atividade.java
@@ -2,10 +2,12 @@ package br.edu.iftm.extensaoSTS.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Atividade implements Serializable {
 
 	private static final long serialVersionUID = 1L;
--- ExtensaoSTS/src/main/java/br/edu/iftm/extensaoSTS/resources/AtividadeResource.java
+++ ExtensaoSTS/src/main/java/br/edu/iftm/extensaoSTS/resources/AtividadeResource.java
@@ -1,22 +1,27 @@
 package br.edu.iftm.extensaoSTS.resources;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.iftm.extensaoSTS.domain.Atividade;
+import br.edu.iftm.extensaoSTS.repositories.IAtividadeRepository;
 
 @RestController
 @RequestMapping(value = "/atividades")
 public class AtividadeResource {
 
+	@Autowired
+	private IAtividadeRepository atividadeRepositorio;
+
 	@GetMapping
-	public List<Atividade> listar(){
-		Atividade a1 = new Atividade(1, "visita tecnica gdg");
-		Atividade a2 = new Atividade(2, "simpós");
-		return Arrays.asList(a1,a2);
+	public List<Atividade> listar() {
+		// Atividade a1 = new Atividade(1, "visita tecnica gdg");
+		// Atividade a2 = new Atividade(2, "simpós");
+		// return Arrays.asList(a1,a2);
+		return atividadeRepositorio.findAll();
 	}
 }
